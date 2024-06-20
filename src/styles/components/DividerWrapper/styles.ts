ee1093553ba399d32ee4e9eb87e5b5a920215c84
Daniel <danielnorges@gmail.com>
2024-06-20 14:13:17 -0300

    feat: new wrapper

--- src/styles/components/DividerWrapper/styles.ts
+++ src/styles/components/DividerWrapper/styles.ts
@@ -1,14 +1,15 @@
 import styled from "styled-components";
+import { Styles } from "../../Styles";
 
 export const ContainerDivider = styled.div`
     max-width: 792px;
-    padding-left: 24px;
-    padding-right: 24px;
-     padding-top: 4rem; 
-    padding-bottom: 4rem; 
+    padding:4rem 1.5rem;
+    @media (${Styles.devices.mobileL}) {
+        padding:4rem 0rem;
+  }
 `;
 
 export const DividerLine = styled.div`
-height: 2px;
+height: 3px;
 background-color: #BAA89B;
 `;
